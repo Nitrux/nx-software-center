24d3997b52b5ce5f06c90148fa6a8a2cef44c369
Alexis López Zubieta <azubieta90@gmail.com>
2017-12-22 13:42:43 -0600

    Add compare by name and version to application class.

--- src/entities/application.h
+++ src/entities/application.h
@@ -4,24 +4,41 @@
 #include <QString>
 
 class Application {
-  QString id;
-  QString name;
-  QString version;
+    QString id;
+    QString version;
 
- public:
-  explicit Application();
-  explicit Application(const QString &name, const QString &version);
+    QString name;
+    QString description;
 
-  QString getId() const;
-  QString getName() const;
-  QString getVersion() const;
+public:
+    explicit Application();
 
-  int Compare(const Application &a) const;
-  bool operator==(const Application &a) const;
-  bool operator<(const Application &a) const;
+    explicit Application(const QString &name, const QString &version);
 
- private:
-  QString generateId(const QString &id, const QString &version) const;
+    QString getId() const;
+
+    QString getVersion() const;
+
+    QString getName() const;
+
+    QString getDescription() const;
+
+    void setDescription(const QString &description);
+
+    int compare(const Application &a) const;
+
+    int compare_by_name(const Application &a) const;
+
+    int compare_by_version(const Application &a) const;
+
+    bool operator==(const Application &a) const;
+
+    bool operator!=(const Application &a) const;
+
+    bool operator<(const Application &a) const;
+
+private:
+    QString generateId(const QString &id, const QString &version) const;
 };
 
 #endif  // APPLICATION_H
