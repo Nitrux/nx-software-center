f0d0ab26b8c28bb450fbbabc181e81a7f8cd8147
Alexis López Zubieta <azubieta90@gmail.com>
2018-02-01 17:06:02 -0600

    Create an appimage build script to find and include qml plugins files.

---
+++ build_scripts/appimage.create.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+echo "## Install into appdir"
+make DESTDIR="appdir" -j$(nproc) install
+find appdir/
+
+echo "Getting linuxdeployqt"
+wget -c -nv "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
+chmod a+x linuxdeployqt-continuous-x86_64.AppImage
+
+# Create a png icon for the AppImage
+wget -c -nv "https://raw.githubusercontent.com/nomad-desktop/luv-icon-theme/master/Luv/apps/64/nx-software-center.svg"
+inkscape -z -e appdir/nx-software-center.png -w 256 -h 256 nx-software-center.svg
+
+bash "`dirname $0`/appimage.include.qml.modules.sh" .. appdir
+
+# Create the appimage
+unset QTDIR; unset QT_PLUGIN_PATH ; unset LD_LIBRARY_PATH
+export VERSION=$(git rev-parse --short HEAD)
+
+./linuxdeployqt-continuous-x86_64.AppImage \
+    appdir/usr/share/applications/*.desktop \
+    -qmldir=/usr/lib/x86_64-linux-gnu/qt5/qml \
+    -bundle-non-qt-libs\
+    -appimage
+
+find appdir/
---
+++ build_scripts/appimage.include.qml.modules.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+#set -x
+
+if [[ $# != 2 ]]; then
+    echo "Missing search path!"
+    echo "Example:
+    bash appimage.include.qml.modules.sh /path/to/your/application/qml/files/base/dir /path/to/appdir"
+    exit 1
+fi
+
+
+echo "## Looking for qml imports in $1"
+modules=$(qmlimportscanner $1 | grep -oE '"name":.*"(.*)"' | cut -d" " -f 2 | sort | uniq)
+modules="${modules//\"/}"
+if [[ -z $modules ]]; then
+    echo "No imports found in $1"
+    exit 0;
+else
+    echo "$modules";
+    echo "";
+fi
+
+
+qmldirs=$(find /usr/lib -iname qmldir)
+qmldirs="$qmldirs $(find /opt -iname qmldir)"
+
+echo "## Matching qmldirs with imports"
+dirs_to_include=""
+for qmldir in $qmldirs; do
+    for module in $modules; do
+        if grep -q "^module $module\$" "$qmldir"; then
+            dirs_to_include="$dirs_to_include `dirname ${qmldir}`"
+            echo "Match $module in $qmldir"
+        fi
+    done
+done
+
+for dir_to_include in $dirs_to_include; do
+    cp -rv $dir_to_include $2/usr/qml/
+done
