3c1cbab1c870584c4366cc7f94ab1c5a5633654c
Alexis López Zubieta <azubieta90@gmail.com>
2018-02-01 19:11:00 -0600

    Search for qml imports only in src/qml

--- build_scripts/appimage.create.sh
+++ build_scripts/appimage.create.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 build_scripts_dir=`dirname $0`
+source_dir=`dirname $build_scripts_dir`
 
 ./$build_scripts_dir/appimage.create.inital.appdir.sh
 
 ./$build_scripts_dir/appimage.create.icon.sh
 
-./$build_scripts_dir/appimage.include.qml.modules.sh .. appdir
+./$build_scripts_dir/appimage.include.qml.modules.sh "$source_dir/src/qml/" appdir
 
 ./$build_scripts_dir/appimage.run.linuxdeployqt.sh
