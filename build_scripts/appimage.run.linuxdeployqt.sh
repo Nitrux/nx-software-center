760260153d6034b014ab8daa4a7daba7725710c2
Alexis López Zubieta <azubieta90@gmail.com>
2018-02-08 11:15:48 -0600

    Use our version of linuxdeployqt

--- build_scripts/appimage.run.linuxdeployqt.sh
+++ build_scripts/appimage.run.linuxdeployqt.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 echo "Getting linuxdeployqt"
-wget -c -nv "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
+wget -c -nv "https://github.com/azubieta/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
 chmod a+x linuxdeployqt-continuous-x86_64.AppImage
 
 # Create the appimage
 unset QTDIR; unset QT_PLUGIN_PATH ; unset LD_LIBRARY_PATH
 export VERSION=$(git rev-parse --short HEAD)
 
-./linuxdeployqt-continuous-x86_64.AppImage \
+LINUX_DEPLOY_QT_EXCLUDE_COPYRIGHTS=true ./linuxdeployqt-continuous-x86_64.AppImage \
     appdir/usr/share/applications/*.desktop \
     -qmldir=src/qml \
     -bundle-non-qt-libs \
