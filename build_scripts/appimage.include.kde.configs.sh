2bcf46e8b96791d29353f172d97c2f920df4d3e1
Alexis López Zubieta <azubieta90@gmail.com>
2018-02-15 17:05:50 -0600

    set kde settings as an application specific rc

---
+++ build_scripts/appimage.include.kde.configs.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+mkdir -p appdir/etc/xdg/
+cp /etc/xdg/kdeglobals appdir/etc/xdg/nx_software_centerrc
--- build_scripts/appimage.include.kde.globals.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p appdir/etc/xdg/
-cp /etc/xdg/kdeglobals appdir/etc/xdg/kdeglobals
