7774797bfdc097bd2b39e319f6c017e60939cb72
Anupam Basak <anupam.basak27@gmail.com>
2018-03-09 00:00:10 +0530

    Updates for running inside nxos/nxos docker image

--- build_scripts/build.sh
+++ build_scripts/build.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-wget -qO - http://archive.neon.kde.org/public.key | sudo apt-key add -
-echo "deb http://archive.neon.kde.org/user/ xenial main" | sudo tee /etc/apt/sources.list.d/neon.list
-wget -qO - http://repo.nxos.org/public.key | sudo apt-key add -
-echo "deb http://repo.nxos.org nxos main" | sudo tee /etc/apt/sources.list.d/nxos.list
+wget -qO - http://archive.neon.kde.org/public.key | apt-key add -
+echo "deb http://archive.neon.kde.org/user/ xenial main" | tee /etc/apt/sources.list.d/neon.list
+wget -qO - http://repo.nxos.org/public.key | apt-key add -
+echo "deb http://repo.nxos.org nxos main" | tee /etc/apt/sources.list.d/nxos.list
 
-sudo apt-get update -qq
+apt-get update -qq
 
-sudo apt-get -y install --allow-unauthenticated inkscape git cmake patchelf extra-cmake-modules qtdeclarative5-dev-tools qml-module-qtgraphicaleffects plasma-framework-dev qtbase5-dev qtdeclarative5-dev libqt5xmlpatterns5-dev nomad-plasma-look-and-feel luv-icon-theme nomad-desktop-settings
+apt-get -y install --allow-unauthenticated curl inkscape git cmake patchelf extra-cmake-modules qtdeclarative5-dev-tools qml-module-qtgraphicaleffects plasma-framework-dev qtbase5-dev qtdeclarative5-dev libqt5xmlpatterns5-dev nomad-plasma-look-and-feel luv-icon-theme nomad-desktop-settings
 
 cmake -DCMAKE_BUILD_TYPE=release -DCMAKE_INSTALL_PREFIX=/usr
 make -j$(nproc)
