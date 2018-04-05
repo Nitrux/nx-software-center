6734ec27ee643ca4873fde10ea9263bb41dcf132
Alexis López Zubieta <azubieta90@gmail.com>
2018-04-04 19:19:39 -0500

    Refactorize appimage.include.qml.modules.py

--- build_scripts/appimage.include.qml.modules.py
+++ build_scripts/appimage.include.qml.modules.py
@@ -38,6 +38,57 @@ def copytree(src, dst, symlinks=False):
         raise shutil.Error(errors)
 
 
+class QmlModulesImporter:
+    def __init__(self):
+        self.qml_source_dir = ""
+        self.app_dir = ""
+        self.qml_module_dirs = ["/usr/lib"]
+
+        self.qmlimportscanner_bin = shutil.which("qmlimportscanner")
+
+    def import_qml_modules(self):
+        print("QML Modules Search paths: ", self.qml_module_dirs)
+        qml_imports = self.scan_qml_imports()
+
+        self.copy_qml_modules(qml_imports)
+
+    def copy_qml_modules(self, qml_imports):
+        qml_imports_included = []
+        for qmlImport in qml_imports:
+            if qmlImport["type"] == "module" and \
+                    "path" in qmlImport and \
+                    qmlImport not in qml_imports_included:
+                source_path = qmlImport["path"]
+                relative_path = qmlImport["relativePath"] if "relativePath" in qmlImport else ""
+                target_path = self.app_dir + "/usr/qml/" + relative_path
+                copytree(source_path, target_path)
+
+                print("Including", qmlImport['name'], qmlImport['version'], "from", source_path)
+                qml_imports_included += qmlImport
+
+    def scan_qml_imports(self):
+        qml_imports=[]
+        for qmlModulesDir in self.qml_module_dirs:
+            raw_output = self.run_qmlimportscanner(qmlModulesDir)
+
+            new_imports = json.loads(raw_output)
+            qml_imports += new_imports
+        return qml_imports
+
+    def run_qmlimportscanner(self, qmlModulesDir):
+        raw_output = ''
+        process = Popen([self.qmlimportscanner_bin,
+                         "-rootPath", self.qml_source_dir,
+                         "-importPath", qmlModulesDir], stdout=PIPE)
+        process.wait()
+        if process.returncode == 0:
+            raw_output = process.stdout.read().decode('utf-8')
+        else:
+            print("ERROR: Unable to run qmlimportscanner")
+            exit(1)
+        return raw_output
+
+
 def main(arguments):
     if len(sys.argv) < 3:
         print(
@@ -45,44 +96,12 @@ def main(arguments):
             "/sourcePath/to/the/AppDir [/paths/to/qml/modules/folders ...]\n")
         exit(1)
 
-    qml_source_dir = sys.argv[1]
-
-    app_dir = sys.argv[2]
-
-    qml_module_dirs = ["/usr/lib"]
-    qml_module_dirs += sys.argv[3:]
-
-    qmlimportscanner_bin = shutil.which("qmlimportscanner")
-
-    print("QML Modules Search paths: ", qml_module_dirs)
-
-    qml_imports = []
-    for qmlModulesDir in qml_module_dirs:
-        process = Popen([qmlimportscanner_bin,
-                         "-rootPath", qml_source_dir,
-                         "-importPath", qmlModulesDir], stdout=PIPE)
-        process.wait()
-
-        if process.returncode == 0:
-            raw_output = process.stdout.read()
-            new_imports = json.loads(raw_output)
-            qml_imports += new_imports
-        else:
-            print("ERROR: Unable to run qmlimportscanner")
-            exit(1)
-
-    qml_imports_included = []
-    for qmlImport in qml_imports:
-        if qmlImport["type"] == "module" and \
-                "path" in qmlImport and \
-                qmlImport not in qml_imports_included:
-            source_path = qmlImport["path"]
-            relative_path = qmlImport["relativePath"] if "relativePath" in qmlImport else ""
-            target_path = app_dir + "/usr/qml/" + relative_path
-            copytree(source_path, target_path)
-
-            print("Including", qmlImport['name'], qmlImport['version'], "from", source_path)
-            qml_imports_included += qmlImport
+    qml_importer = QmlModulesImporter()
+    qml_importer.qml_source_dir = sys.argv[1]
+    qml_importer.app_dir = sys.argv[2]
+    qml_importer.qml_module_dirs += sys.argv[3:]
+    qml_importer.import_qml_modules()
+    return 0
 
 
 if __name__ == '__main__':
