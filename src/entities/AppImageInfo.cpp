acacd02262fc9227455bfde598334a73166dde5f
Alexis López Zubieta <azubieta90@gmail.com>
2018-07-03 14:05:55 -0500

    Create new AppImageInfo entity to hold the metadata of installed applications.

---
+++ src/entities/AppImageInfo.cpp
@@ -0,0 +1,264 @@
+//
+// Created by alexis on 7/3/18.
+//
+
+#include "AppImageInfo.h"
+AppImageInfo AppImageInfo::fromVariant(const QVariant& variant)
+{
+    auto map = variant.toMap();
+    if (!map.isEmpty()) {
+        AppImageInfo a;
+        a.id = map["id"].toString();
+        a.name = LocalizedQString::fromVariant(map["name"]);
+        a.icon = map["icon"].toString();
+        a.abstract = LocalizedQString::fromVariant(map["abstract"]);
+        a.description = LocalizedQString::fromVariant(map["description"]);
+        a.license = License::fromVariant(map["license"]);
+        a.categories = map["categories"].toStringList();
+        a.keywords = map["keywords"].toStringList();
+        a.languages = map["languages"].toStringList();
+        a.developer = Developer::fromVariant(map["developer"]);
+        a.release = Release::fromVariant(map["release"]);
+        a.file = File::fromVariant(map["file"]);
+
+        QList<RemoteImage> screenshots;
+        for (const auto& v: map["screenshots"].toList()) {
+            screenshots << RemoteImage::fromVariant(v);
+        }
+        a.screenshots = screenshots;
+
+        a.mimeTypes = map["mime-types"].toStringList();
+
+        QMap<QString, QString> links;
+        auto linksVMap = map["links"].toMap();
+        for (const auto& k: linksVMap.keys())
+            links[k] = linksVMap[k].toString();
+
+        a.links = links;
+        return a;
+    }
+    return AppImageInfo();
+}
+QVariant AppImageInfo::toVariant(const AppImageInfo& appImageInfo)
+{
+    QVariantMap map;
+    map["id"] = appImageInfo.id;
+    map["name"] = LocalizedQString::toVariant(appImageInfo.name);
+    map["icon"] = appImageInfo.icon;
+    map["abstract"] = LocalizedQString::toVariant(appImageInfo.abstract);
+    map["description"] = LocalizedQString::toVariant(appImageInfo.description);
+
+
+    return map;
+}
+bool AppImageInfo::operator==(const AppImageInfo& rhs) const
+{
+    return id==rhs.id &&
+            name==rhs.name &&
+            icon==rhs.icon &&
+            abstract==rhs.abstract &&
+            description==rhs.description &&
+            license==rhs.license &&
+            categories==rhs.categories &&
+            keywords==rhs.keywords &&
+            languages==rhs.languages &&
+            developer==rhs.developer &&
+            release==rhs.release &&
+            file==rhs.file &&
+            screenshots==rhs.screenshots &&
+            mimeTypes==rhs.mimeTypes &&
+            links==rhs.links;
+}
+bool AppImageInfo::operator!=(const AppImageInfo& rhs) const
+{
+    return !(rhs==*this);
+}
+
+bool AppImageInfo::License::operator==(const AppImageInfo::License& rhs) const
+{
+    return id==rhs.id &&
+            name==rhs.name &&
+            body==rhs.body;
+}
+bool AppImageInfo::License::operator!=(const AppImageInfo::License& rhs) const
+{
+    return !(rhs==*this);
+}
+std::ostream& operator<<(std::ostream& os, const AppImageInfo::License& license)
+{
+    os << "{ id: " << license.id.toStdString() << " name: " << license.name.toStdString()
+       << " body: " << license.body.toStdString() << " }";
+    return os;
+}
+AppImageInfo::License AppImageInfo::License::fromVariant(QVariant& variant)
+{
+    License l;
+    QVariantMap map = variant.toMap();
+    l.id = map["id"].toString();
+    l.name = map["name"].toString();
+    l.body = map["body"].toString();
+    return l;
+}
+bool AppImageInfo::Developer::operator==(const AppImageInfo::Developer& rhs) const
+{
+    return name==rhs.name &&
+            avatar==rhs.avatar &&
+            website==rhs.website &&
+            pubkey==rhs.pubkey;
+}
+bool AppImageInfo::Developer::operator!=(const AppImageInfo::Developer& rhs) const
+{
+    return !(rhs==*this);
+}
+std::ostream& operator<<(std::ostream& os, const AppImageInfo::Developer& developer)
+{
+    os << "{ name: " << developer.name.toStdString() << " avatar: " << developer.avatar.toStdString()
+       << " website: " << developer.website.toStdString() << " pubkey: " << developer.pubkey.toStdString() << " }";
+    return os;
+}
+AppImageInfo::Developer AppImageInfo::Developer::fromVariant(QVariant& variant)
+{
+    auto map = variant.toMap();
+    AppImageInfo::Developer developer;
+    developer.name = map["name"].toString();
+    developer.avatar = map["avatar"].toString();
+    developer.website = map["website"].toString();
+    developer.pubkey = map["pubkey"].toString();
+
+    return developer;
+}
+bool AppImageInfo::Release::operator==(const AppImageInfo::Release& rhs) const
+{
+    return date==rhs.date &&
+            version==rhs.version &&
+            channel==rhs.channel &&
+            changelog==rhs.changelog;
+}
+bool AppImageInfo::Release::operator!=(const AppImageInfo::Release& rhs) const
+{
+    return !(rhs==*this);
+}
+std::ostream& operator<<(std::ostream& os, const AppImageInfo::Release& release)
+{
+    os << "{ date: " << release.date.toString().toStdString() << " version: " << release.version.toStdString()
+       << " channel: " << release.channel.toStdString() << " changelog: " << release.changelog << " }";
+    return os;
+}
+AppImageInfo::Release AppImageInfo::Release::fromVariant(const QVariant& variant)
+{
+    auto map = variant.toMap();
+    AppImageInfo::Release release;
+    release.version = map["version"].toString();
+    release.date = map["date"].toDateTime();
+    release.channel = map["channel"].toString();
+    release.changelog = LocalizedQString::fromVariant(map["changelog"]);
+
+    return release;
+}
+bool AppImageInfo::File::operator==(const AppImageInfo::File& rhs) const
+{
+    return type==rhs.type &&
+            size==rhs.size &&
+            architecture==rhs.architecture &&
+            sha512checksum==rhs.sha512checksum &&
+            remote_url==rhs.remote_url && local_url==rhs.local_url;
+}
+bool AppImageInfo::File::operator!=(const AppImageInfo::File& rhs) const
+{
+    return !(rhs==*this);
+}
+std::ostream& operator<<(std::ostream& os, const AppImageInfo::File& file)
+{
+    os << "{ type: " << file.type << " size: " << file.size
+       << " architecture: " << file.architecture.toStdString()
+       << " sha512checksum: " << file.sha512checksum.toStdString()
+       << " remote_url: " << file.remote_url.toStdString() << " local_url: " << file.local_url.toStdString() << " }";
+    return os;
+}
+AppImageInfo::File::File()
+        :type(0), size(0) { }
+AppImageInfo::File AppImageInfo::File::fromVariant(const QVariant& variant)
+{
+    auto map = variant.toMap();
+    AppImageInfo::File file;
+    file.type = map["type"].toInt();
+    file.size = map["size"].toInt();
+    file.architecture = map["architecture"].toString();
+    file.sha512checksum = map["sha512checksum"].toString();
+    file.local_url = map["local_url"].toString();
+    file.remote_url = map["remote_url"].toString();
+
+    return file;
+}
+bool AppImageInfo::RemoteImage::operator==(const AppImageInfo::RemoteImage& rhs) const
+{
+    return height==rhs.height &&
+            width==rhs.width &&
+            caption==rhs.caption &&
+            url==rhs.url;
+}
+bool AppImageInfo::RemoteImage::operator!=(const AppImageInfo::RemoteImage& rhs) const
+{
+    return !(rhs==*this);
+}
+std::ostream& operator<<(std::ostream& os, const AppImageInfo::RemoteImage& image)
+{
+    os << "{ height: " << image.height << " width: " << image.width
+       << " caption: " << image.caption.toStdString() << " url: " << image.url.toStdString() << " }";
+    return os;
+}
+AppImageInfo::RemoteImage::RemoteImage()
+        :height(0), width(0) { }
+AppImageInfo::RemoteImage AppImageInfo::RemoteImage::fromVariant(const QVariant& variant)
+{
+    AppImageInfo::RemoteImage remoteImage;
+    auto map = variant.toMap();
+    remoteImage.url = map["url"].toString();
+    remoteImage.caption = map["caption"].toString();
+    remoteImage.height = map["height"].toInt();
+    remoteImage.width = map["width"].toInt();
+    remoteImage.language = map["language"].toString();
+    return remoteImage;
+}
+std::ostream& operator<<(std::ostream& os, const AppImageInfo::LocalizedQString& string)
+{
+    os << "{ ";
+    for (auto key: string.keys())
+        os << key.toStdString() << ": " << string[key].toStdString() << ", ";
+    os << " }";
+    return os;
+}
+AppImageInfo::LocalizedQString AppImageInfo::LocalizedQString::fromVariant(const QVariant& variant)
+{
+    auto localizedStringVariant = variant.toMap();
+    LocalizedQString localizedQString;
+    for (const auto& k: localizedStringVariant.keys())
+        localizedQString[k] = localizedStringVariant[k].toString();
+    return localizedQString;
+}
+QVariant AppImageInfo::LocalizedQString::toVariant(const AppImageInfo::LocalizedQString& string)
+{
+    QVariantMap map;
+    for (const auto& key: string.keys())
+        map[key] = string[key];
+    return map;
+}
+std::ostream& operator<<(std::ostream& os, const AppImageInfo& info)
+{
+    os << "id: " << info.id.toStdString() << " name: " << info.name
+       << " icon: " << info.icon.toStdString() << " abstract: " << info.abstract
+       << " description: " << info.description << " license: " << info.license
+       << " categories: " << info.categories.join(", ").toStdString()
+       << " keywords: " << info.keywords.join(", ").toStdString()
+       << " languages: " << info.languages.join(", ").toStdString()
+       << " developer: " << info.developer
+       << " release: " << info.release << " file: " << info.file;
+    os << " screenshots: ";
+    for (const auto& screenshot: info.screenshots)
+        os << screenshot << ", ";
+    os << " mimeTypes: " << info.mimeTypes.join(", ").toStdString();
+    os << " links: ";
+    for (auto key: info.links.keys())
+        os << key.toStdString() << ": " << info.links[key].toStdString() << ", ";
+    return os;
+}
---
+++ src/entities/AppImageInfo.h
@@ -0,0 +1,119 @@
+//
+// Created by alexis on 7/3/18.
+//
+
+#ifndef NX_SOFTWARE_CENTER_APPIMAGEINFO_H
+#define NX_SOFTWARE_CENTER_APPIMAGEINFO_H
+
+#include <QMap>
+#include <QList>
+#include <QString>
+#include <QDateTime>
+#include <QVariant>
+#include <ostream>
+
+class AppImageInfo {
+public:
+    class LocalizedQString : public QMap<QString, QString> {
+    public:
+        friend std::ostream& operator<<(std::ostream& os, const LocalizedQString& string);
+
+        static LocalizedQString fromVariant(const QVariant &variant);
+        static QVariant toVariant(const LocalizedQString &string);
+    };
+
+    class License {
+    public:
+        QString id;
+        QString name;
+        QString body;
+
+        bool operator==(const License& rhs) const;
+        bool operator!=(const License& rhs) const;
+        friend std::ostream& operator<<(std::ostream& os, const License& license);
+        static License fromVariant(QVariant& variant);
+    };
+
+    class Developer {
+    public:
+        QString name;
+        QString avatar;
+        QString website;
+        QString pubkey;
+
+        bool operator==(const Developer& rhs) const;
+        bool operator!=(const Developer& rhs) const;
+        friend std::ostream& operator<<(std::ostream& os, const Developer& developer);
+        static Developer fromVariant(QVariant& variant);
+    };
+
+    class Release {
+    public:
+        QDateTime date;
+        QString version;
+        QString channel;
+        LocalizedQString changelog;
+        bool operator==(const Release& rhs) const;
+        bool operator!=(const Release& rhs) const;
+        friend std::ostream& operator<<(std::ostream& os, const Release& release);
+        static Release fromVariant(const QVariant& variant);
+    };
+
+    class File {
+    public:
+        int type;
+        int size;
+        QString architecture;
+        QString sha512checksum;
+        QString remote_url;
+        QString local_url;
+
+        File();
+        bool operator==(const File& rhs) const;
+        bool operator!=(const File& rhs) const;
+        friend std::ostream& operator<<(std::ostream& os, const File& file);
+        static File fromVariant(const QVariant& variant);
+    };
+
+    class RemoteImage {
+    public:
+        int height;
+        int width;
+        QString language;
+        QString caption;
+        QString url;
+
+        RemoteImage();
+        bool operator==(const RemoteImage& rhs) const;
+        bool operator!=(const RemoteImage& rhs) const;
+        friend std::ostream& operator<<(std::ostream& os, const RemoteImage& image);
+        static RemoteImage fromVariant(const QVariant& variant);
+    };
+
+    QString id;
+    LocalizedQString name;
+    QString icon;
+    LocalizedQString abstract;
+    LocalizedQString description;
+
+    License license;
+    QStringList categories;
+    QStringList keywords;
+    QStringList languages;
+    Developer developer;
+    Release release;
+    File file;
+    QList<RemoteImage> screenshots;
+    QStringList mimeTypes;
+    QMap<QString, QString> links;
+
+    static AppImageInfo fromVariant(const QVariant& variant);
+    static QVariant toVariant(const AppImageInfo& appImageInfo);
+
+    bool operator==(const AppImageInfo& rhs) const;
+    bool operator!=(const AppImageInfo& rhs) const;
+    friend std::ostream& operator<<(std::ostream& os, const AppImageInfo& info);
+
+};
+
+#endif //NX_SOFTWARE_CENTER_APPIMAGEINFO_H
