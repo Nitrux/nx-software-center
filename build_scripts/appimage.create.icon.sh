afb16893793f7d8f1e1bf02fbd97c751c6387267
Camilo Higuita <chiguitar@unal.edu.co>
2019-09-13 11:26:44 +0200

    delte file for brand new start

--- build_scripts/appimage.create.icon.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# Create a png icon for the AppImage
-wget -c -nv "https://raw.githubusercontent.com/Nitrux/luv-icon-theme/master/Luv/apps/64/nx-software-center.svg"
-rsvg-convert nx-software-center.svg -w 256 -h 256 >> appdir/nx-software-center.png
