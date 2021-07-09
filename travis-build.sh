a665198a3356ba34cd0c28c5669af06acb4df67b
Uri Herrera <hello@uriherrera.com>
2021-07-09 06:46:14 -0500

    fix dep name
    - deps only available in neon unstable repo

--- travis-build.sh
+++ travis-build.sh
@@ -16,7 +16,7 @@ apt -qq update
 
 ### Install Dependencies
 
-DEBIAN_FRONTEND=noninteractive apt -qq -yy install devscripts debhelper gettext lintian build-essential automake autotools-dev cmake extra-cmake-modules mauikit-dev libmauikit-filebrowsing-dev
+DEBIAN_FRONTEND=noninteractive apt -qq -yy install devscripts debhelper gettext lintian build-essential automake autotools-dev cmake extra-cmake-modules mauikit-dev libmauikitfilebrowsing-dev
 mk-build-deps -i -t "apt-get --yes" -r
 
 ### Build Deb
