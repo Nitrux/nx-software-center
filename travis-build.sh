17cf88388e2fd9ac772eea2133ad7089652c703f
Uri Herrera <hello@uriherrera.com>
2021-07-09 06:42:58 -0500

    fix name of repo

--- travis-build.sh
+++ travis-build.sh
@@ -7,23 +7,16 @@ apt -qq -yy install equivs curl git wget gnupg2
 
 ### FIXME - the container mauikit/ubuntu-18.04-amd64 does have the neon repo but for some idiotic reason it isn't working here
 
-wget -qO /etc/apt/sources.list.d/neon-unstable-repo.list https://raw.githubusercontent.com/Nitrux/iso-tool/development/configs/files/sources.list.neon.user
+wget -qO /etc/apt/sources.list.d/neon-unstable-repo.list https://raw.githubusercontent.com/Nitrux/iso-tool/development/configs/files/sources.list.neon.unstable
 
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys \
 	55751E5D > /dev/null
-
-###	Add Nitrux repo, needed for mauikit filebrowsing
-
-wget -qO /etc/apt/sources.list.d/nitrux-repo.list https://raw.githubusercontent.com/Nitrux/iso-tool/development/configs/files/
-sources.list.nitrux 
-
-curl -L https://packagecloud.io/nitrux/repo/gpgkey | apt-key add -;
 
 apt -qq update
 
 ### Install Dependencies
 
-DEBIAN_FRONTEND=noninteractive apt -qq -yy install devscripts debhelper gettext lintian build-essential automake autotools-dev cmake extra-cmake-modules
+DEBIAN_FRONTEND=noninteractive apt -qq -yy install devscripts debhelper gettext lintian build-essential automake autotools-dev cmake extra-cmake-modules mauikit-dev libmauikit-filebrowsing-dev
 mk-build-deps -i -t "apt-get --yes" -r
 
 ### Build Deb
