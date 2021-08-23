8d76b7e4245e8e18c5bdacc3c0b643ae6be211cc
Camilo Higuita <chiguitar@unal.edu.co>
2021-08-23 10:20:15 -0500

    flags clenup

--- src/main.cpp
+++ src/main.cpp
@@ -31,9 +31,7 @@
 int main(int argc, char *argv[])
 {
 	QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-	QCoreApplication::setAttribute(Qt::AA_DontCreateNativeWidgetSiblings);
 	QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps, true);
-	QCoreApplication::setAttribute(Qt::AA_DisableSessionManager, true);
 
 #ifdef Q_OS_ANDROID
 	QGuiApplication app(argc, argv);
