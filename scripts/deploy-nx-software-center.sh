bc5be8187aa0895e906f841da9c63cf229451cd9
Probal Basak <proba31@gmail.com>
2021-09-23 14:35:38 +0530

    Changes: - Removed mauikit image tools build script - Removed nx sc clone inside build folder and use the already cloned repo

--- scripts/deploy-nx-software-center.sh
+++ scripts/deploy-nx-software-center.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-git clone https://github.com/Nitrux/nx-software-center.git
-
-cd nx-software-center
+mkdir nx-software-center && cd nx-software-center
 cmake -DCMAKE_BUILD_TYPE="Release" \
     -DCMAKE_INSTALL_PREFIX="/usr" \
-    .
+    ../../
 
 make -j$(nproc)
 make install
