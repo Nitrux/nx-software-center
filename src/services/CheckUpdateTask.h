78956fa4a55de1588f3a1fc0f993575aef2bd07b
Probal Basak <probal31@gmail.com>
2021-11-16 16:18:09 +0530

    Created Check Update Task

---
+++ src/services/CheckUpdateTask.h
@@ -0,0 +1,28 @@
+#pragma once
+// system
+
+// libraries
+#include <QAppImageUpdate>
+#include <QObject>
+
+// local
+#include "TaskManager.h"
+
+class CheckUpdateTask : public Task
+{
+    Q_OBJECT
+public:
+    CheckUpdateTask(const QString &id, const QString &appImagePath, const QString &appName, QObject *parent);
+    Q_SCRIPTABLE void start();
+
+protected:
+    Q_SLOT void onWorkerStarted(short);
+    Q_SLOT void onWorkerFinished(QJsonObject info, short);
+    Q_SLOT void onWorkerProgress(int, qint64, qint64, double, QString, short);
+    Q_SLOT void onWorkerError(short, short);
+
+private:
+    QString _appName;
+
+    QAppImageUpdate *_worker;
+};
