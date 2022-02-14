bc4d7873169f609053a0437c5aab8eec9218b19a
Probal Basak <probal31@gmail.com>
2022-02-14 13:45:54 +0530

    Removed unused code

--- src/models/applicationmanager.cpp
+++ src/models/applicationmanager.cpp
@@ -24,16 +24,6 @@ void ApplicationManager::getApplications(QList<QString> categoriesFilter, QStrin
     if ( category->categoryStore == Category::CategoryStore::APPIMAGEHUB ) {
         // Invoke appimagehub api
         m_appimagehubStore->getApplicationsByArch(categoriesFilter, nameFilter, sortMode, page, pageSize, tags, arch);
-
-        // On clicking a top level category (appimagehub category), fetch the apps from apprepo sub-categories as well
-        // which is there under the clicked top level category. So that all apps from appimagehub and apprepo are shown
-        // foreach (Category *item, category->categories) {
-        //     if ( item->categoryStore == Category::CategoryStore::APPREPO ) {
-        //         if ( page=="0" || page=="" ) {
-        //             m_apprepoStore->getPackagesByGroup(item->id.toInt());
-        //         }
-        //     }
-        // }
     } else if ( category->categoryStore == Category::CategoryStore::APPREPO ) {
         // Invoke apprepo api
         if ( page=="0" || page=="" ) {
@@ -42,9 +32,5 @@ void ApplicationManager::getApplications(QList<QString> categoriesFilter, QStrin
     } else {
         // Invoke appimagehub api
         m_appimagehubStore->getApplicationsByArch(categoriesFilter, nameFilter, sortMode, page, pageSize, tags, arch);
-
-        // This is not required because no apprepo section is getting displayed in store home
-        // Invoke apprepo api
-        // m_apprepoStore->getPackages();
     }
 }
