06b8adf8b70405bdc22830a0df9644b4debad6c2
Uri Herrera <hello@uriherrera.com>
2022-12-01 01:46:17 -0600

    update pkg

--- scripts/build-deb.sh
+++ scripts/build-deb.sh
@@ -3,20 +3,15 @@
 set -x
 
 ### Update sources
-wget -qO /etc/apt/sources.list.d/neon-user-repo.list https://raw.githubusercontent.com/Nitrux/iso-tool/development/configs/files/sources.list.neon.user
+wget -qO /etc/apt/sources.list.d/nitrux-main-compat-repo.list https://raw.githubusercontent.com/Nitrux/iso-tool/development/configs/files/sources/nitrux-repo.list
 
-wget -qO /etc/apt/sources.list.d/nitrux-main-compat-repo.list https://raw.githubusercontent.com/Nitrux/iso-tool/development/configs/files/sources.list.nitrux
-
-wget -qO /etc/apt/sources.list.d/nitrux-testing-repo.list https://raw.githubusercontent.com/Nitrux/iso-tool/development/configs/files/sources.list.nitrux.testing
-
-DEBIAN_FRONTEND=noninteractive apt-key adv --keyserver keyserver.ubuntu.com --recv-keys \
-	55751E5D > /dev/null
+wget -qO /etc/apt/sources.list.d/nitrux-testing-repo.list https://raw.githubusercontent.com/Nitrux/iso-tool/development/configs/files/sources/nitrux-testing-repo.list
 
 curl -L https://packagecloud.io/nitrux/repo/gpgkey | apt-key add -;
 curl -L https://packagecloud.io/nitrux/compat/gpgkey | apt-key add -;
 curl -L https://packagecloud.io/nitrux/testing/gpgkey | apt-key add -;
 
-DEBIAN_FRONTEND=noninteractive apt -qq update
+apt -qq update
 
 ### Install Package Build Dependencies #2
 ### NX Software Center needs ECM > 5.70
@@ -49,6 +44,8 @@ cmake \
 
 make -j$(nproc)
 
+make install
+
 ### Run checkinstall and Build Debian Package
 
 >> description-pak printf "%s\n" \
@@ -71,7 +68,7 @@ checkinstall -D -y \
 	--pakdir=. \
 	--maintainer=uri_herrera@nxos.org \
 	--provides=nx-software-center \
-	--requires="libappimage1.0 \(\>= 1.0.3\),libc6,libgcc-s1,libgcrypt20,libkf5coreaddons5,libkf5i18n5,libqt5core5a,libqt5gui5,libqt5network5,libqt5qml5,libqt5widgets5,libstdc++6,mauikit-git \(\>= 2.2.0+git\),mauikit-accounts-git \(\>= 2.2.0+git\),mauikit-filebrowsing-git \(\>= 2.2.0+git\),qml-module-qt-labs-platform,qml-module-qtwebview,zsync2,libappimageupdate" \
+	--requires="libappimage1.0 \(\>= 1.0.3\),libc6,libgcc-s1,libgcrypt20,libkf5coreaddons5,libkf5i18n5,libqt5core5a,libqt5gui5,libqt5network5,libqt5qml5,libqt5widgets5,libstdc++6,mauikit-git \(\>= 2.2.1+git\),mauikit-accounts-git \(\>= 2.2.1+git\),mauikit-filebrowsing-git \(\>= 2.2.1+git\),qml-module-qt-labs-platform,qml-module-qtwebview,zsync2,libappimageupdate" \
 	--nodoc \
 	--strip=no \
 	--stripso=yes \
