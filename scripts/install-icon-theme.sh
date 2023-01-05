6f79e67849eeb17cfb898afb94a41e0daed16370
Probal Basak <probal31@gmail.com>
2023-01-05 17:55:57 +0530

    Changes: - Update to latest mauikit release versions - Filebrowsing use master since fix provided after release which is in master - Install appimage builder from master till fix for pydpkg is released

--- scripts/install-icon-theme.sh
+++ scripts/install-icon-theme.sh
@@ -3,5 +3,5 @@
 git clone https://github.com/Nitrux/luv-icon-theme.git --depth=1
 
 cd luv-icon-theme
-cp -vrf Luv/* "$APPDIR"/usr/share/icons/hicolor/
-rm "$APPDIR"/usr/share/icons/hicolor/icon-theme.cache
+cp -vrf Luv/* "$TARGET_APPDIR"/usr/share/icons/hicolor/
+rm "$TARGET_APPDIR"/usr/share/icons/hicolor/icon-theme.cache
