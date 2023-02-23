7a5955efcae1b3dfa2d4b323758058bfb9db80df
UriHerrera <hello@uriherrera.com>
2023-02-22 18:19:13 -0600

    [ci skip]

--- scripts/build-deb.sh
+++ scripts/build-deb.sh
@@ -19,11 +19,11 @@ apt -qq update
 ### NX Software Center needs CMake > 3.19
 
 apt -qq -yy install --no-install-recommends \
+	libappimage-dev/trixie \
 	mauikit-accounts-git \
 	mauikit-filebrowsing-git \
 	mauikit-git \
-    zsync2 \
-	libappimage-dev/trixie
+	zsync2
 
 rm -rf {LICENSE,README.md}
 
@@ -71,7 +71,7 @@ checkinstall -D -y \
 	--pakdir=. \
 	--maintainer=uri_herrera@nxos.org \
 	--provides=nx-software-center \
-	--requires="libappimage1.0abi1 \(\>= 1.0.4\),libc6,libgcc-s1,libgcrypt20,libkf5coreaddons5,libkf5i18n5,libqt5core5a,libqt5gui5,libqt5network5,libqt5qml5,libqt5widgets5,libstdc++6,mauikit-git \(\>= 2.2.2+git\),mauikit-accounts-git \(\>= 2.2.2+git\),mauikit-filebrowsing-git \(\>= 2.2.2+git\),qml-module-qt-labs-platform,qml-module-qtwebview,zsync2,libappimageupdate" \
+	--requires="libappimage1.0 \(\>= 1.0.4\),libc6,libgcc-s1,libgcrypt20,libkf5coreaddons5,libkf5i18n5,libqt5core5a,libqt5gui5,libqt5network5,libqt5qml5,libqt5widgets5,libstdc++6,mauikit-git \(\>= 2.2.2+git\),mauikit-accounts-git \(\>= 2.2.2+git\),mauikit-filebrowsing-git \(\>= 2.2.2+git\),qml-module-qt-labs-platform,qml-module-qtwebview,zsync2,libappimageupdate" \
 	--nodoc \
 	--strip=no \
 	--stripso=yes \
