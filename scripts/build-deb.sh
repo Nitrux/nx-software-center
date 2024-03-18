e81f0331f3753bbfb43b0cd92482729c6e0f738b
UriHerrera <hello@uriherrera.com>
2024-03-18 13:06:36 -0600

    [ci skip]

--- scripts/build-deb.sh
+++ scripts/build-deb.sh
@@ -4,42 +4,13 @@ set -x
 
 ### Update sources
 
-wget -qO /etc/apt/sources.list.d/nitrux-depot.list https://raw.githubusercontent.com/Nitrux/iso-tool/legacy/configs/files/sources/sources.list.nitrux
-
+wget -qO /etc/apt/sources.list.d/nitrux-depot.list https://raw.githubusercontent.com/Nitrux/iso-tool/legacy/configs/files/sources/sources.list.nitrux.depot
 wget -qO /etc/apt/sources.list.d/nitrux-testing.list https://raw.githubusercontent.com/Nitrux/iso-tool/legacy/configs/files/sources/sources.list.nitrux.testing
+wget -qO /etc/apt/sources.list.d/nitrux-unison.list https://raw.githubusercontent.com/Nitrux/iso-tool/legacy/configs/files/sources/sources.list.nitrux.unison
 
 curl -L https://packagecloud.io/nitrux/depot/gpgkey | apt-key add -;
-curl -L https://packagecloud.io/nitrux/unison/gpgkey | apt-key add -;
 curl -L https://packagecloud.io/nitrux/testing/gpgkey | apt-key add -;
-
-apt -qq update
-
-### Update GCC #1
-### Use the libappimage package from Mantic because the version in Jammy is too old, and AppImageUpdate fails to compile due to "undefined references."
-### And also update GCC and CPP because the version of libappimage in Mantic is compiled against GCC 12.2 and CPP 12.
-
->> ubuntu-mantic.list printf "%s\n" \
-    '#################' \
-    '# Ubuntu Mantic #' \
-    '################' \
-    '' \
-    'deb [arch=amd64] http://archive.ubuntu.com/ubuntu mantic main restricted universe multiverse' \
-    'deb [arch=amd64] http://archive.ubuntu.com/ubuntu mantic-security main restricted universe multiverse' \
-    'deb [arch=amd64] http://archive.ubuntu.com/ubuntu mantic-updates main restricted universe multiverse' \
-    ''
-
-mv ubuntu-mantic.list /etc/apt/sources.list.d/
-
-apt -qq update
-
-apt -qq -yy install --no-install-recommends --only-upgrade \
-	gcc
-
-apt -qq -yy install --no-install-recommends \
-	libappimage-dev
-
-rm -r \
-	/etc/apt/sources.list.d/ubuntu-mantic.list
+curl -L https://packagecloud.io/nitrux/unison/gpgkey | apt-key add -;
 
 apt -qq update
 
@@ -57,7 +28,6 @@ apt -qq -yy -o Dpkg::Options::="--force-overwrite" install --no-install-recommen
 rm -rf {LICENSE,README.md}
 
 ### Compile Source
-### Note: Use curl =< 7.81.0 because libcpr will fail to compile against newer versions of curl due to libcpr using a deprecated API call; see https://github.com/libcpr/cpr/issues/870
 
 mkdir -p build && cd build
 
